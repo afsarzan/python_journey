41319ba4a0f7c1b18976f80163d8e77c40e4a064
afsarzan <afsar_zan@yahoo.com>
2025-06-03 21:12:46 -0700

    Chore: basic syntax for functions and math functions

--- index.py
+++ index.py
@@ -1,2 +1,23 @@
-name = "Afsar"
-print(name)
+name = "Afsar is my name"
+# print(name)
+# this is comment
+
+
+def greet(name):
+    """Function to greet a person with their name."""
+    print(f"Hello, {name}!")
+
+def get_expected_cost(beds, baths):
+    value = 80000 + (beds*30000) + (baths * 10000)
+    return value
+
+print(get_expected_cost(3, 2))
+
+
+def get_cost(sqft_walls, sqft_ceiling, sqft_per_gallon, cost_per_gallon):
+    total_sqft = sqft_walls + sqft_ceiling
+    gallons_needed = total_sqft / sqft_per_gallon
+    cost = cost_per_gallon * gallons_needed
+    # round the cost to 2 decimal places
+    return round(cost, 2)
+print(get_cost(1000, 500, 350, 25))
