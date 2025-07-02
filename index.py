5e6a7e2cf50d1de6808296a645809859b50bb03b
afsarzan <afsar_zan@yahoo.com>
2025-07-02 00:44:37 -0700

    chore: Covered core concepts 2

--- index.py
+++ index.py
@@ -166,3 +166,17 @@ print(f"Sorted merged list: {sorted}")  # Merge and sort two lists
 #sort combined list
 print(sorted(list1 + list2))  # Sort the combined list
 print((list1 + list2))  # Concatenate two lists
+
+
+#dictionaries
+point = {"x": 1, "y": 2}
+point = dict (x = 1 , y = 2 )
+point["x"] = 10
+point["z"] = 20
+if "a" in point:
+    print(point ["a"])
+print(point.get("a", 0))
+del point ["x"]
+print(point)
+for key in point:
+ print(key, point[key])
