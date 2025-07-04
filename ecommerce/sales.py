467215e5efa8fe7af6846f34ec98c0fcfa407b2d
afsarzan <afsar_zan@yahoo.com>
2025-07-04 13:16:47 -0700

    chore: Covered core concepts - packages

---
+++ ecommerce/sales.py
@@ -0,0 +1,6 @@
+def calc_shipping():
+    print("Calculating shipping...")
+
+def calc_tax():
+    print("Calculating tax...")
+
---
+++ modules.py
@@ -0,0 +1,6 @@
+from ecommerce.sales import calc_shipping, calc_tax
+import sys
+calc_shipping()
+calc_tax()  
+
+# print(sys.path)
