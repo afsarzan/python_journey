674aed8e03c07b9673b1b8515af3c35e619ec4a0
afsarzan <afsar_zan@yahoo.com>
2025-07-03 20:06:30 -0700

    chore: Covered core concepts - properties

--- core.py
+++ core.py
@@ -32,3 +32,23 @@ print(cloud.tags)  # Display all tags and their counts
 # Iterating through the tags
 for tag, count in cloud:
     print(f"{tag}: {count}")
+
+#using properties
+class Product:
+    def __init__(self, price):
+        self.set_price(price)
+    
+    @property
+    def price(self):
+        return self._price
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError("Price cannot be negative.")
+        self._price = value
+
+product = Product(100)
+print(product.price)  # Accessing the price using the property
+product.price = 150  # Setting a new price using the property
+print(product.price)  # Accessing the updated price
+product.price = -50  # Uncommenting this line will raise a ValueError
